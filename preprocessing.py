ee524b9acf4ce575bb3920b78542239b39df0bf9
steven0129 <steven0129>
2019-11-02 11:52:07 +0800

    Optimize preprocessing process

--- preprocessing.py
+++ preprocessing.py
@@ -13,15 +13,16 @@ class FeatureHashing():
         self.conam_enc = MinMaxScaler()
         self.label_encs = None
         self.onehot_enc = OneHotEncoder(categories='auto')
-        self.svd = TruncatedSVD(n_components=1000, algorithm='arpack')
+        self.svd = TruncatedSVD(n_components=5000, algorithm='arpack')
+        self._fit()
 
-    def training_set(self):
+    def _fit(self):
         train_set = pd.read_csv(self.train_set)
         test_set = pd.read_csv(self.test_set)
         
         Y = train_set['fraud_ind']
         X = train_set.drop(columns=['fraud_ind']).append(test_set, ignore_index=True)
-        conam = self.conam_enc.fit_transform(X['conam'].values.reshape(-1, 1)).astype('float16')
+        self.conam_enc.fit(X['conam'].values.reshape(-1, 1)).astype('float16')
         X = X.drop(columns=['txkey', 'locdt', 'conam'])
         X = X.astype(str)
 
@@ -29,13 +30,33 @@ class FeatureHashing():
         self.label_encs = dict(zip(keys, [LabelEncoder() for _ in keys]))
 
         for key in keys:
-            X[key] = self.label_encs[key].fit_transform(X[key].values)
+            self.label_encs[key].fit(X[key].values)
 
         X = self._hash_func(X)
-        X = self.onehot_enc.fit_transform(X)
-        X = self.svd.fit_transform(X)
+        self.onehot_enc.fit(X)
+        self.svd.fit(X)
+
+        return self
+
+    def training_set(self):
+        train_set = pd.read_csv(self.train_set)
+        
+        Y = train_set['fraud_ind']
+        X = train_set.drop(columns=['fraud_ind'])
+        conam = self.conam_enc.transform(X['conam'].values.reshape(-1, 1)).astype('float16')
+        X = X.drop(columns=['txkey', 'locdt', 'conam'])
+        X = X.astype(str)
+
+        keys = list(X.keys())
+        self.label_encs = dict(zip(keys, [LabelEncoder() for _ in keys]))
+
+        for key in keys:
+            X[key] = self.label_encs[key].transform(X[key].values)
+
+        X = self._hash_func(X)
+        X = self.onehot_enc.transform(X)
+        X = self.svd.transform(X)
         X = hstack((X, conam))
-        X = X[:train_set.shape[0], :]
 
         return X, Y
 
